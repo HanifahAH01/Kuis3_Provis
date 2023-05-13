d02c5d62538c9511ef80e607dd7397b261774776
Amzar123 <aji.muhammad.zapar080500@gmail.com>
2023-05-13 11:12:58 +0700

    fetch data from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,48 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
-import 'second_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer' as developer;
 
-void main() {
-  runApp(const MyApp());
+class ActivityModel {
+  String id;
+  String nama;
+  String jenis;
+  ActivityModel(
+      {required this.id,
+      required this.nama,
+      required this.jenis}); //constructor
 }
+
+class ActivityCubit extends Cubit<List<ActivityModel>> {
+  String url = "http://178.128.17.76:8000/daftar_umkm";
+  ActivityCubit() : super([]);
+
+  List<ActivityModel> ListActivityModel = <ActivityModel>[];
+  //map dari json ke atribut
+  void setFromJson(Map<String, dynamic> json) {
+    var data = json["data"];
+    for (var val in data) {
+      String id = val['id'];
+      String nama = val['nama'];
+      String jenis = val['jenis'];
+
+      ListActivityModel.add(ActivityModel(id: id, nama: nama, jenis: jenis));
+    }
+    emit(ListActivityModel);
+  }
+
+  void fetchData() async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      setFromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Gagal load');
+    }
+  }
+}
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,31 +50,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test Route',
-      routes: {
-        '/': (context) => const MyHome(),
-        '/layar2': (context) => const LayarKedua()
-      },
+      home: BlocProvider(
+        create: (_) => ActivityCubit(),
+        child: const HalamanUtama(),
+      ),
     );
   }
 }
 
-//perlu  dipisah karena Navigator perlu punya parent Material App
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
+class HalamanUtama extends StatelessWidget {
+  const HalamanUtama({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Test Route'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocBuilder<ActivityCubit, List<ActivityModel>>(
+                // ignore: non_constant_identifier_names
+                builder: (context, ListActivityModel) {
+                  return Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read<ActivityCubit>().fetchData();
+                        },
+                        child: const Text("Reload Daftar UMKM"),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      if (ListActivityModel.isNotEmpty)
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: ListActivityModel.length,
+                          itemBuilder: (context, index) {
+                            var activity = ListActivityModel[index];
+                            return Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(),
+                              ),
+                              padding: const EdgeInsets.all(14),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(activity.id.toString()),
+                                  Text(activity.nama),
+                                  Text(activity.jenis),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      if (ListActivityModel.isEmpty)
+                        const Text('Data tidak ditemukan')
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-          child: ElevatedButton(
-              child: const Text('Ke screen Kedua'),
-              onPressed: () {
-                Navigator.of(context).pushNamed("/layar2");
-              })),
     );
   }
 }
